7f46302629672ae68b6cb54af318d64576133c78
unknown <ayyavuraj01@gmail.com>
2022-12-06 09:19:19 +0530

    Add Files

---
+++ src/test/java/org/demo/Ayyavu.java
@@ -0,0 +1,20 @@
+package org.demo;
+
+public class Ayyavu {
+	
+	
+	private void Tamil() {
+		System.out.println("Tamil");
+
+	}
+	private void English() {
+		System.out.println("English");
+
+	}
+	private void Telugu() {
+		System.out.println("Telugu");
+
+	}
+		
+
+}
